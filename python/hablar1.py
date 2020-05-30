144da011f5ff12b8e04d04e5f5d15c1bb2b00f08
Miguel Barraza <miguelbarraza2015@gmail.com>
2020-05-30 15:20:05 -0300

    ejemplos en python

---
+++ python/hablar1.py
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# prueba de accesible_output2 en python 3.x
+
+import accessible_output2.outputs.auto
+
+engine = accessible_output2.outputs.auto.Auto()
+
+print("mensaje que se imprime en consola")
+
+engine.speak("hola, este mensaje sera leído por un lector de pantalla.", True)
+engine.speak("El string es transformado a voz.", False)
---
+++ python/hablar3.py
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# prueba de accesible_output2 en python 3.x
+
+import accessible_output2.outputs.auto
+from time import sleep
+
+engine = accessible_output2.outputs.auto.Auto()
+
+print("mensaje que se imprime en consola")
+
+sleep(0.1)
+
+engine.speak("hola, este mensaje sera leído por un lector de pantalla.", True)
+engine.speak("El string es transformado a voz.", True)
